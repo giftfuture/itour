d38d706c9bc06a3fa62b250e3611b4285b92e1f9
THUNDEROBOT@Immanuel_Fred <zpchoney@gmail.com>
2016-09-30 16:01:58 +0800

    f

---
+++ src/com/itour/controller/front/HappyController.java
@@ -0,0 +1,43 @@
+package com.itour.controller.front;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.itour.base.web.BaseController;
+import com.itour.entity.Customers;
+import com.itour.page.CustomersPage;
+import com.itour.service.CustomersService;
+
+@Controller
+@RequestMapping("/happy") 
+public class HappyController  extends BaseController{
+	
+	private final static Logger log= Logger.getLogger(HappyController.class);
+	
+	// Servrice start
+	@Autowired(required=false) //自动注入，不需要生成set方法了，required=false表示没有实现类，也不会报错。
+	private CustomersService<Customers> customersService; 
+	
+	/**
+	 * 
+	 * @param url
+	 * @param classifyId
+	 * @return
+	 * @throws Exception 
+	 */
+	@RequestMapping("/list") 
+	public ModelAndView list(CustomersPage page,HttpServletRequest request) throws Exception{
+	/*	Map<String,Object>  context = getRootMap();
+		//page.setDeleted(DELETED.NO.key);
+		List<Customers> dataList = customersService.queryByList(page);
+		//设置页面数据
+		context.put("dataList", dataList);*/
+		return forword("server/sys/customers"); 
+	}
+	
+}
